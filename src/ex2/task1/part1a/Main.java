df7bcaa23f7691de750efa658674a214a69d5db1
Finleif <58663789+Finleif@users.noreply.github.com>
2022-09-14 20:18:58 +0200

    task 1a done

--- src/ex2/task1/part1a/Main.java
+++ src/ex2/task1/part1a/Main.java
@@ -1,7 +1,17 @@
 package ex2.task1.part1a;
 
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        // TODO: TASK
+
+        List<String> listen = Arrays.asList("10", "1", "20", "110", "21", "12");
+
+        Collections.sort(listen, (a,b) -> {
+            return a.compareTo(b);
+        });
+
+        System.out.println(listen);
+
     }
 }
