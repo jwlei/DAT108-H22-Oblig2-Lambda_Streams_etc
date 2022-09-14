3a1db9921956b84b4c700f2450a45dfb8289ddcb
jwlei <joleiros@gmail.com>
2022-09-14 23:35:58 +0200

    Task 2 DONE

--- src/ex2/task2/Main.java
+++ src/ex2/task2/Main.java
@@ -30,7 +30,51 @@ public class Main {
                 new Employee("Lise",    "Aanstad",  Gender.FEMALE,  "FrontEnd",     600_000),
                 new Employee("Jurgen",  "Hansen",   Gender.MALE,    "FrontEnd",     500_000)
         );
-    printAll(employeeList);
+
+        // Function to increase salary by INCREASED_SALARY;
+        Function<Employee, Integer> increasedSalary =
+                employee -> employee.getSalary() + INCREASED_SALARY;
+
+        // Function to increase salary by PERCENTAGE_INCREASE;
+        Function<Employee, Integer> increasedByPercentage =
+                employee -> employee.getSalary() + (employee.getSalary()*PERCENTAGE_INCREASE/100);
+
+        // Function to increase salaries < 600_000 by INCREASED_LOW_SALARY
+        Function<Employee, Integer> increaseLowSalary =
+                employee -> employee.getSalary() < 600_000 ? (employee.getSalary() + INCREASED_LOW_SALARY) : employee.getSalary();
+
+        // Function to increase salary by PERCENTAGE_INCREASE_MALE if the employee is MALE;
+        Function<Employee, Integer> increaseForMale =
+                employee -> employee.getGender() == Gender.MALE ? (employee.getSalary() + employee.getSalary()*PERCENTAGE_INCREASE_MALE/100) : employee.getSalary();
+
+        // Base list
+        printAll(employeeList);
+
+        // Increase all salaries by 50_000
+        System.out.println("Increase all salaries by 50_000");
+        salaryAdjustment(employeeList, increasedSalary);
+        printAll(employeeList);
+        System.out.println();
+
+        // Increase all salaries by 7%
+        System.out.println("Increase all salaries by 7%");
+        salaryAdjustment(employeeList, increasedByPercentage);
+        printAll(employeeList);
+        System.out.println();
+
+        // Increase salaries < 600_000 by INCREASED_LOW_SALARY
+        System.out.println("Increase salaries < 600_000 by INCREASED_LOW_SALARY");
+        salaryAdjustment(employeeList, increaseLowSalary);
+        printAll(employeeList);
+        System.out.println();
+
+        // Increase salary by 15% if the employee is MALE
+        System.out.println("Increase salary by 15% if the employee is MALE");
+        salaryAdjustment(employeeList, increaseForMale);
+        printAll(employeeList);
+        System.out.println();
+
+
     }
 
     // TODO: Create functions for adjustments of salary
@@ -43,11 +87,12 @@ public class Main {
      * R apply.(T t);
      */
 
-    Function<Employee, Integer> increasedSalary =
-            employee -> employee.getSalary() + INCREASED_SALARY;
+
+
+
 
     private static void salaryAdjustment(List<Employee> employeeList, Function<Employee, Integer> function) {
-
+        employeeList.forEach(employee -> employee.setSalary(function.apply(employee)));
     }
 
     private static void printAll(List<Employee> employeeList) {
@@ -55,5 +100,6 @@ public class Main {
          * A bit redundant, but it looks nice when calling the print all
          */
         System.out.print(employeeList);
+        System.out.println();
     }
 }
