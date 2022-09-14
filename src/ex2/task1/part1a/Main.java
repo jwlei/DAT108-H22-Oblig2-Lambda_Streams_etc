396a7ae09f51139319e7cbcd932aca6ce7268563
jwlei <joleiros@gmail.com>
2022-09-14 21:13:14 +0200

    Task 1A Done

--- src/ex2/task1/part1a/Main.java
+++ src/ex2/task1/part1a/Main.java
@@ -1,7 +1,48 @@
 package ex2.task1.part1a;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        // TODO: TASK
+        List<String> intList = Arrays.asList("10", "1", "20", "110", "21", "12");
+
+        /**
+         * Sort the list of Strings with in ascending order based on the Integer value
+         * Utilize a lambda expression as a comparator in the Collections.sort() method.
+         *
+         * Basic lambda:
+         * (x, y) -> x.Attribute().compareTo(y.Attribute());
+         */
+
+        /*
+         * Collections.sort(list, new Comparator<Integer>() {
+         * @Override
+         * public int compare(String s1, String s2) {
+         *     return s1.compareTo(s2);
+         *     }
+         *  });
+         * ----------------- OR -----------------
+         * Collections.sort(list, String::compareTo);
+         *
+         */
+
+        /*
+         * Collections.sort(listToSort, (compareX, compareY) -> {
+         *      type x = typeCast.compareX
+         *      type y = typeCast.compareY
+         *      return x.compareTo(y)
+         * }
+         */
+
+        Collections.sort(intList, (x, y) -> {
+            Integer x1 = Integer.parseInt(x);
+            Integer y1 = Integer.parseInt(y);
+
+            return x1.compareTo(y1);
+        });
+        System.out.println(intList);
     }
 }
