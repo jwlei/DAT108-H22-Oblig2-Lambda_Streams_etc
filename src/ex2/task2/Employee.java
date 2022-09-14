65a054dcbf24235151bedd029923d68d806270e6
jwlei <joleiros@gmail.com>
2022-09-14 22:59:38 +0200

    Task 2 WIP

---
+++ src/ex2/task2/Employee.java
@@ -0,0 +1,73 @@
+package ex2.task2;
+
+public class Employee {
+
+    String fName, lName, position;
+    Gender gender;
+    int salary;
+
+    public Employee(String fName, String lName, Gender gender, String position, int salary) {
+        /**
+         * Constructor
+         */
+        this.fName = fName;
+        this.lName = lName;
+        this.gender = gender;
+        this.position = position;
+        this.salary = salary;
+    }
+
+    // Getters and setters
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    // toString
+    @Override
+    public String toString() {
+        /**
+         * toString method to print all employees
+         */
+        return "Employee [Name = " + fName
+                + ", Lastname = " + lName
+                + ", Gender = " + gender
+                + ", position = " + position
+                + ", salary = "+ salary + "]\n";
+    }
+}
--- src/ex2/task2/Main.java
+++ src/ex2/task2/Main.java
@@ -1,7 +1,59 @@
 package ex2.task2;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
 public class Main {
+    // TODO: Fast kronetillegg
+    private final static int INCREASED_SALARY = 50_000;
+
+    // TODO: fast prosenttillegg
+    private final static int PERCENTAGE_INCREASE = 7;
+
+    // TODO: fast kronetillegg for lav lønn
+    private final static int INCREASED_LOW_SALARY = 30_000;
+
+    // TODO: fast prosentttillegg hvis du er mann
+    private final static int PERCENTAGE_INCREASE_MALE = 15;
     public static void main(String[] args) {
-        // TODO: TASK
+
+        /*
+         * Create a list with dummy data to perform operations on
+         */
+        List<Employee> employeeList = Arrays.asList(
+                new Employee("Tom",     "Hansen",   Gender.MALE,    "Boss",         1_000_000),
+                new Employee("Kari",    "Nielsen",  Gender.THEY,    "Sales",        500_000),
+                new Employee("Ole",     "Karlsen",  Gender.MALE,    "Sales",        500_000),
+                new Employee("Per",     "Crowo",    Gender.MALE,    "FullStack",    700_000),
+                new Employee("Mona",    "Aarsen",   Gender.FEMALE,  "BackEnd",      500_000),
+                new Employee("Lise",    "Aanstad",  Gender.FEMALE,  "FrontEnd",     600_000),
+                new Employee("Jurgen",  "Hansen",   Gender.MALE,    "FrontEnd",     500_000)
+        );
+    printAll(employeeList);
+    }
+
+    // TODO: Create functions for adjustments of salary
+
+    /*
+     * A Function<T, R>
+     * T - Input
+     * R - Result
+     *
+     * R apply.(T t);
+     */
+
+    Function<Employee, Integer> increasedSalary =
+            employee -> employee.getSalary() + INCREASED_SALARY;
+
+    private static void salaryAdjustment(List<Employee> employeeList, Function<Employee, Integer> function) {
+
+    }
+
+    private static void printAll(List<Employee> employeeList) {
+        /*
+         * A bit redundant, but it looks nice when calling the print all
+         */
+        System.out.print(employeeList);
     }
 }
