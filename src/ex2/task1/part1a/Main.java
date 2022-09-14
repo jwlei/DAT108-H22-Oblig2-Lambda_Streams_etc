4344479ead58091d7ad654d825ce2de036c5b525
jwlei <joleiros@gmail.com>
2022-09-14 19:25:50 +0200

    git init

---
+++ src/ex2/task1/part1a/Main.java
@@ -0,0 +1,7 @@
+package ex2.task1.part1a;
+
+public class Main {
+    public static void main(String[] args) {
+        // TODO: TASK
+    }
+}
---
+++ src/ex2/task3/Main.java
@@ -0,0 +1,7 @@
+package ex2.task3;
+
+public class Main {
+    public static void main(String[] args) {
+        // TODO: TASK
+    }
+}
