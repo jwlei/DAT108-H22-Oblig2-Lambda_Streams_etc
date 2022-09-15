15ec77f3af240733e46de179ca135713ebcebd9b
h587727 <587727@stud.hvl.no>
2022-09-15 13:19:49 +0200

    Test

--- src/ex2/task1/part1a/Main.java
+++ src/ex2/task1/part1a/Main.java
@@ -5,14 +5,7 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-        List<String> listen = Arrays.asList("10", "1", "20", "110", "21", "12");
-
-
-        Collections.sort(listen, (a,b) -> {
-            return a.compareTo(b);
-        });
-
-        System.out.println(listen);
+  System.out.println("test");
 
     }
 }
