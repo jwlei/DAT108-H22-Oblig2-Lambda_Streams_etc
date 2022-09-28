274a7e0ac7229b4d2bc4e52480ca6286cf5777c2
jwlei <joleiros@gmail.com>
2022-09-28 19:32:51 +0200

    Started task 4, refactored to english

---
+++ src/ex2/task4/FindKingApplication.java
@@ -0,0 +1,13 @@
+package ex2.task4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindKingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindKingApplication.class, args);
+	}
+
+}
